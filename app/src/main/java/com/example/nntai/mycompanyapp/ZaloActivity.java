a9c5b9be9369ce81c7c91140eedee26b314c4317
michaelDnguyen <nguyennhattai07@gmail.com>
2018-04-09 11:27:58 +0700

    second first =))))

---
+++ app/src/main/java/com/example/nntai/mycompanyapp/ZaloActivity.java
@@ -0,0 +1,14 @@
+package com.example.nntai.mycompanyapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ZaloActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zalo);
+    }
+
+}
